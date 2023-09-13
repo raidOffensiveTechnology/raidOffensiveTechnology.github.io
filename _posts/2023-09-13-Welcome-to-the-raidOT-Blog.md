7d2bcd2d5fa6de2c10762d7e9feed81222abfb25
p-i-c-o <95228665+p-i-c-o@users.noreply.github.com>
2023-09-13 19:01:55 +0200

    added intro

---
+++ _posts/2023-09-13-Welcome-to-the-raidOT-Blog.md
@@ -0,0 +1,9 @@
+---
+title: Welcome to the raidOT Blog
+categories: ['misc']
+author: me
+---
+# Welcome!
+This is the raidOT Hacking Club Blog, here you will find all the tutorials and writeups to all the hacking needs you have!
+
+### Enjoy!
---
+++ _posts/generate.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import os
+
+def get_current_date():
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    return current_date
+
+def create(filename, content):
+    with open(filename, 'w+') as f:
+	     f.write(content)
+	     f.close()
+
+date = get_current_date()
+title = input('Article title >>> ')
+tags =  input('Article tags  >>> ').split(' ')
+
+template = f"""---
+title: {title}
+categories: {tags}
+author: me
+---
+"""
+
+filen = date+"-"+title.replace(' ', '-')+".md"
+
+create(filen, template)
+
+os.system(f'nano {filen}')
